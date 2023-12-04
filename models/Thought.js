f5155e37194fc55d002b2bc22dea7014c2c20d35
Evanilla57 <grigg005@cougars.csusm.edu>
2023-12-03 16:54:46 -0800

    built User model, started Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,10 @@
-const ReactionSchema = new Schema(
+const { ObjectId } = require("bson");
+const moment = require("moment");
+const { Schema, model, Types } = require("mongoose");
 
+//Schema to create Thought model
+const ReactionSchema = new Schema(
+    
 );
 
 const ThoughtSchema = new Schema(
--- models/User.js
+++ models/User.js
@@ -1,3 +1,50 @@
+const { Schema, model } = require("mongoose");
+const { isEmail } = require("validator");
+
+// Schema to create User model
 const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            require: true,
+            validate: [isEmail, "invalid email"],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "user",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
 );
 
+// Virtual property for friend count
+userSchema.virtual("friendCount").get(function () {
+    return `${this.friends.length}`;
+});
+
+// Initialize User Model
+const User = model("user", userSchema);
+
+// Exports User Model
+
+module.exports = User;
